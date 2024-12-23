65dba7fa77da02e95b5d36e2d74a5aabdb1f19bf
RichieQuintana <richi3quintana@gmail.com>
2024-12-23 11:22:10 -0500

    deployar

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\holaq\OneDrive\Documents\flask_project\venv
+set VIRTUAL_ENV=C:\Users\holaq\OneDrive\Documents\Proyecto\flask_project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
